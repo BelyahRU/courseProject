6a1b02aaef663950c696fad17f99f09521d10ff4
BelyahRU <oBelyash@yandex.ru>
2023-04-24 19:19:46 +0300

    make my option

--- courseProject/main.swift
+++ courseProject/main.swift
@@ -1,11 +1,8 @@
-//
-//  main.swift
-//  courseProject
-//
-//  Created by Александр Андреев on 24.04.2023.
-//
-
-import Foundation
-
-print("Hello, World!")
+// 626 - last 3 digits of student card
+// 1) subject area - Регистрация постояльцев в гостинице
+// 2) hashing method - Закрытое хеширование с квадратичным опробованрием
+// 3) sorting method - Шейкерная
+// 4) list type - Линейный двунаправленный
+// 5) tree traversal method - Прямой
+// 6) word search algorithm in the text - Боуера и Мура (БМ)
 
